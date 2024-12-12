bd7c5787222baa7ae72ea83faa88d580ccc8044d
Sergei <sergega@me.com>
2024-12-12 14:18:28 +0300

    doxygen add

--- lib/avl.c
+++ lib/avl.c
@@ -1,27 +1,17 @@
-/* Produced by texiweb from libavl.w. */
-
-/* libavl - library for manipulation of binary trees.
-   Copyright (C) 1998-2002, 2004 Free Software Foundation, Inc.
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-   See the GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-   02111-1307, USA.
-
-   The author may be contacted at <blp@gnu.org> on the Internet, or
-   write to Ben Pfaff, Stanford University, Computer Science Dept., 353
-   Serra Mall, Stanford CA 94305, USA.
-*/
+/**
+ * @file avl.c
+ * @brief Библиотека для работы с AVL-деревьями.
+ *
+ * AVL-деревья — это сбалансированные бинарные деревья поиска, предоставляющие
+ * операции вставки, удаления, поиска и обхода элементов с временной сложностью O(log n).
+ *
+ * @details
+ * Этот заголовочный файл содержит структуры данных и функции для управления AVL-деревьями,
+ * включая базовые операции с деревьями, управление памятью и итерацию по дереву.
+ *
+ * Copyright 1998-2002, 2004 Free Software Foundation, Inc.
+ * Licensed under the GNU General Public License (GPL) version 2 or later.
+ */
 
 #include <assert.h>
 #include <stdio.h>
