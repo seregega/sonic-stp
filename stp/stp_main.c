8c8494cfd85843c1d0eedf47eeec646bf41c269e
wbos_44 <sergega@me.com>
2025-03-14 13:45:02 +0300

    fix

--- stp/stp_main.c
+++ stp/stp_main.c
@@ -414,7 +414,7 @@ int stpd_main()
         return -1;
     }
 
-    const char test_messages[]  {
+    const char test_messages[] = {
         "Hello WBOS from stpd with automatic debugi"};
 
         stpd_context.send_resp_ipc_packet(&stpd_context,test_messages, sizeof(test_messages));
